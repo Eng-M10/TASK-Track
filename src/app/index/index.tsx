48d625a854c2f48b6d84a1ea9421dd3c052c3e7f
Muvimbene Maposse <muvi10.com@gmail.com>
2025-10-14 09:49:45 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,16 +1,16 @@
 import { FloatingButton } from '@/components/FAB';
 import { colors } from '@/constants/colors';
 import { MaterialIcons } from '@expo/vector-icons';
+import notifee, { AndroidImportance } from '@notifee/react-native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { router } from 'expo-router';
+import { useEffect } from 'react';
 import { Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Doing } from './(tabs)/doing';
 import { Done } from './(tabs)/done';
 import { Todo } from './(tabs)/todo';
 import { styles } from './styles';
-import { useEffect } from 'react';
-import notifee, { AndroidImportance } from '@notifee/react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -31,43 +31,45 @@ export default function Index() {
       body: 'Você tem uma tarefa pendente. Não se esqueça de concluí-la!',
       android: {
         channelId: chanelId,
-        smallIcon: 'icon',
+        smallIcon: 'ic_launcher',
+        color: colors.gray[400]
       }
     });
-
-    useEffect(() => {
-      displaynotification();
-    }, []);
-
-
-
-
-    return (
-      <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.titleContainer}><Text style={styles.title}>Tasks</Text></View>
-          <TouchableOpacity>
-            <MaterialIcons size={32} name="search" color={colors.blue[800]} />
-          </TouchableOpacity>
-        </View>
-        <View style={{ flex: 1 }}>
-          <Tab.Navigator
-            screenOptions={{
-              tabBarStyle: { backgroundColor: colors.gray[400] },
-              tabBarIndicatorStyle: { backgroundColor: colors.blue[800] },
-            }}
-          >
-            <Tab.Screen
-              name="To-do" component={Todo} />
-            <Tab.Screen
-              name="Doing" component={Doing} />
-            <Tab.Screen
-              name="Done" component={Done} />
-          </Tab.Navigator>
-        </View>
-
-        <FloatingButton onPress={() => { router.navigate("/add") }} />
-      </SafeAreaView>
-    );
-
   }
+
+  useEffect(() => {
+    displaynotification();
+  }, []);
+
+
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.titleContainer}><Text style={styles.title}>Tasks</Text></View>
+        <TouchableOpacity>
+          <MaterialIcons size={32} name="search" color={colors.blue[800]} />
+        </TouchableOpacity>
+      </View>
+      <View style={{ flex: 1 }}>
+        <Tab.Navigator
+          screenOptions={{
+            tabBarStyle: { backgroundColor: colors.gray[400] },
+            tabBarIndicatorStyle: { backgroundColor: colors.blue[800] },
+          }}
+        >
+          <Tab.Screen
+            name="To-do" component={Todo} />
+          <Tab.Screen
+            name="Doing" component={Doing} />
+          <Tab.Screen
+            name="Done" component={Done} />
+        </Tab.Navigator>
+      </View>
+
+      <FloatingButton onPress={() => { router.navigate("/add") }} />
+    </SafeAreaView>
+  );
+
+}
